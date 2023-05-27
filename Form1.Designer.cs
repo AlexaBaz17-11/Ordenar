ef9aea039d3f5c05404037eb783e880bf027e980
JAKOB DYLAN <JAKOB DYLAN@LAPTOP-S1INHMDR>
2023-05-26 21:36:16 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Ordenar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxInsertar = new System.Windows.Forms.TextBox();
+            this.Números = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBoxBuscar = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBoxEliminar = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.Equation;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBoxInsertar);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.groupBox1.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(22, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 83);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese Número";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Turquoise;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(123, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxInsertar
+            // 
+            this.textBoxInsertar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBoxInsertar.Location = new System.Drawing.Point(28, 28);
+            this.textBoxInsertar.Multiline = true;
+            this.textBoxInsertar.Name = "textBoxInsertar";
+            this.textBoxInsertar.Size = new System.Drawing.Size(76, 42);
+            this.textBoxInsertar.TabIndex = 0;
+            // 
+            // Números
+            // 
+            this.Números.BackColor = System.Drawing.Color.Black;
+            this.Números.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Números.ForeColor = System.Drawing.Color.Aqua;
+            this.Números.FormattingEnabled = true;
+            this.Números.ItemHeight = 22;
+            this.Números.Location = new System.Drawing.Point(12, 116);
+            this.Números.Name = "Números";
+            this.Números.Size = new System.Drawing.Size(160, 202);
+            this.Números.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(178, 156);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(182, 118);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ordenar Lista";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Orange;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(18, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 31);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Descendente";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orange;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(18, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ascendente";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxBuscar);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(384, 115);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(170, 185);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ingrese Número";
+            // 
+            // textBoxBuscar
+            // 
+            this.textBoxBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBoxBuscar.Location = new System.Drawing.Point(45, 66);
+            this.textBoxBuscar.Multiline = true;
+            this.textBoxBuscar.Name = "textBoxBuscar";
+            this.textBoxBuscar.Size = new System.Drawing.Size(76, 42);
+            this.textBoxBuscar.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Turquoise;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(31, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 35);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Buscar";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBoxEliminar);
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Font = new System.Drawing.Font("Modern No. 20", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(43, 334);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(369, 93);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Ingrese Número a Eliminar";
+            // 
+            // textBoxEliminar
+            // 
+            this.textBoxEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.textBoxEliminar.Location = new System.Drawing.Point(53, 28);
+            this.textBoxEliminar.Multiline = true;
+            this.textBoxEliminar.Name = "textBoxEliminar";
+            this.textBoxEliminar.Size = new System.Drawing.Size(76, 42);
+            this.textBoxEliminar.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Turquoise;
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(135, 37);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 33);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Eliminar";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(600, 430);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Números);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.Crimson;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxInsertar;
+        private System.Windows.Forms.ListBox Números;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxBuscar;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBoxEliminar;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ordenar
+{
+    public partial class Form1 : Form
+    {
+        private List<int> numeros = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private List<int> listaNumeros = new List<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string numero = textBoxInsertar.Text;
+            try
+            {
+                int parsedNumber = ParseNumero(numero);
+                numeros.Add(parsedNumber);
+                Números.Items.Add(parsedNumber);
+                textBoxInsertar.Clear();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ingrese un número válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private int ParseNumero(string numero)
+        {
+            if (int.TryParse(numero, out int parsedNumber))
+            {
+                return parsedNumber;
+            }
+            else
+            {
+                throw new FormatException();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OrdenarAscendente(numeros);
+            ActualizarListBox();
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OrdenarDescendente(numeros);
+            ActualizarListBox();
+        }
+        private void OrdenarAscendente(List<int> lista)
+        {
+            lista.Sort();
+        }
+
+        private void OrdenarDescendente(List<int> lista)
+        {
+            lista.Sort();
+            lista.Reverse();
+        }
+
+        private void ActualizarListBox()
+        {
+            Números.Items.Clear();
+            Números.Items.AddRange(numeros.Select(n => (object)n).ToArray());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int numeroBuscado = int.Parse(textBoxBuscar.Text);
+                int indice = BuscarNumero(numeroBuscado);
+
+                if (indice != -1)
+                {
+                    Números.SelectedIndex = indice;
+                    MessageBox.Show($"El número {numeroBuscado} se encuentra en la posición {indice}.");
+                }
+                else
+                {
+                    MessageBox.Show($"El número {numeroBuscado} no se encuentra en la lista.");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ingresa un número válido.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Se produjo un error: {ex.Message}");
+            }
+            textBoxBuscar.Clear(); // Limpiar el textBoxBuscar
+        }
+
+        private int BuscarNumero(int numero)
+        {
+            for (int i = 0; i < Números.Items.Count; i++)
+            {
+                int numeroActual = (int)Números.Items[i];
+                if (numeroActual == numero)
+                {
+                    return i; // Se encontró el número en la posición i
+                }
+            }
+
+            return -1; // El número no se encontró en la lista
+        }
+        private int BuscarNumeroRecursivo(int numero, int inicio, int fin)
+        {
+            if (inicio > fin)
+            {
+                return -1; // El número no se encontró en la lista
+            }
+
+            int medio = (inicio + fin) / 2;
+            int numeroMedio = (int)Números.Items[medio];
+
+            if (numero == numeroMedio)
+            {
+                return medio; // Se encontró el número en la posición medio
+            }
+            else if (numero < numeroMedio)
+            {
+                return BuscarNumeroRecursivo(numero, inicio, medio - 1); // Buscar en la mitad izquierda
+            }
+            else
+            {
+                return BuscarNumeroRecursivo(numero, medio + 1, fin); // Buscar en la mitad derecha
+            }
+        }
+        private void EliminarNumero(int numero)
+        {
+            bool eliminado = false;
+
+            for (int i = Números.Items.Count - 1; i >= 0; i--)
+            {
+                if (Convert.ToInt32(Números.Items[i]) == numero)
+                {
+                    Números.Items.RemoveAt(i);
+                    eliminado = true;
+                }
+            }
+
+            if (eliminado)
+            {
+                MessageBox.Show($"El número {numero} ha sido eliminado de la lista.");
+            }
+            else
+            {
+                MessageBox.Show($"El número {numero} no se encuentra en la lista.");
+            }
+
+            textBoxEliminar.Clear();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int numeroEliminar = int.Parse(textBoxEliminar.Text);
+                EliminarNumero(numeroEliminar);
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ingresa un número válido.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Se produjo un error: {ex.Message}");
+            }
+        }
+
+        private void MostrarLista()
+        {
+            Números.Items.Clear();
+            foreach (var numero in listaNumeros)
+            {
+                Números.Items.Add(numero);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
